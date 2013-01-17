6a0dbab396ebc5d4c035c4d6ad36bb08eb1c4bc7
Vitor Navarro <vitor.navarro87@gmail.com>
2013-01-17 00:42:21 -0200

    Adicionando arquivo de dados do jogo

---
+++ assets/data.js
@@ -0,0 +1,10 @@
+game_data = [{"code":1,"name":"level 1","map":[
+	["S","S","S","S"],
+	["S","S","S","S"],
+	["S","S","S","S"],
+	["S","S","S","S"],
+	["S","S","S","S"],
+]},
+{"code":2,"name":"level 2","map":[
+]}
+]
--- level.js
+++ level.js
@@ -11,7 +11,7 @@ function Level(){
 	for (var i = this.columns - 1; i >= 0; i--) {
 		var col_blocks = new Array();
 		for (var j = this.rows - 1; j >= 0; j--) {			
-			var chain = new Chain("img/stoneblock.png",32,i,j);						
+			var chain = new Chain("assets/stoneblock.png",32,i,j);						
 			chain.setAsConnector(0,9)
 			col_blocks.push(chain);	
 		};		
